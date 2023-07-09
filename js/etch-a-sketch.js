b0e1493e7761110f097ec77a3af07ef8c4663f63
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-07-09 15:31:19 +0100

    Etch-a-sketch code works, takes a user prompt after pressing the Start and creates a grid that takes the same amount of page space reglardless of size.

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,24 +1,44 @@
 const gridContainer = document.querySelector(".board-grid");
+const startButtonContainer = document.querySelector(".start-button-container");
+const startButton = document.querySelector(".start-button");
 
-function createGrid() {
-    for(let i = 0; i < 16; i++) {
+// Functions
+function setGridSize() {
 
-        const gridRow = document.createElement("div")
-        gridRow.classList.add("grid-row")
-        
+    let gridSize = prompt("Grid size: ");
 
-        for(let j = 0; j < 16; j++) {
+    if (gridSize > 100) {
+        alert("ERROR: width/height must be smaller than 100.")
+    } else if (gridSize == null) {
+        alert("Cancelled")  
+    } else if (isNaN(gridSize) || gridSize == "") {
+        alert("ERROR: Incorrect values");
+    } else {
+        gridSize = Number(gridSize);
+        createGrid(gridSize);
+    }
+}
+
+function createGrid(gridSize) {
+
+    startButtonContainer.removeChild(startButton);
+
+    for(let i = 0; i < gridSize; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+        gridContainer.appendChild(gridRow);
+    
+        for(let j = 0; j < gridSize; j++) {
             const gridBlock = document.createElement("div");
             gridBlock.classList.add("grid-block");
             gridRow.appendChild(gridBlock);
-            gridContainer.appendChild(gridRow);
-
-            gridBlock.addEventListener("mouseover", function (e) {
-            console.log(e);
-            gridBlock.classList.add("hover-colour");
-});
+            gridBlock.addEventListener("mouseover", function () {
+                gridBlock.classList.add("hover-colour");
+            });
         }
     }
 }
 
-createGrid();
+startButton.addEventListener("click", function (e) {
+    setGridSize();
+});
